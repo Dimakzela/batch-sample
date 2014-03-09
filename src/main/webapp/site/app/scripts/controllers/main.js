f6c2b757fe97d6bc550d738996bb498636dee0bf
Eduardo Diaz <iamedu@gmail.com>
2014-03-09 13:56:45 -0600

    Subiendo todo

--- src/main/webapp/site/app/scripts/controllers/main.js
+++ src/main/webapp/site/app/scripts/controllers/main.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('siteApp')
-  .controller('MainCtrl', function ($scope) {
-    jQuery(".dropzone").dropzone();
+  .controller('MainCtrl', function ($scope, $http) {
+    var loadSummary = function () {
+      $http.get('/batch-sample/data.json').success(function (data) {
+        $scope.summary = data;
+      });
+    };
+
+    jQuery(".dropzone").dropzone({
+      success: function () {
+        loadSummary();
+      }
+    });
+
+    loadSummary();
   });
