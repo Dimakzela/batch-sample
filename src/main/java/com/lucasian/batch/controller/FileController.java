4e8ae95e94e970b40d254efe6c5409267924a06e
Eduardo Diaz <iamedu@gmail.com>
2014-03-09 11:43:02 -0600

    Carga de tabajo de spring batch

--- src/main/java/com/lucasian/batch/controller/FileController.java
+++ src/main/java/com/lucasian/batch/controller/FileController.java
@@ -1,6 +1,17 @@
 package com.lucasian.batch.controller;
 
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+
 import java.util.*;
+
+import java.io.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +24,26 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping(value = "/file")
 public class FileController {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    @Qualifier("resumenJob")
+    private Job resumenJob;
+
     @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = "application/json")
-    public @ResponseBody String uploadFile(@RequestParam MultipartFile file) {
+    public @ResponseBody String uploadFile(@RequestParam MultipartFile file) throws Exception {
+        File dest = File.createTempFile("lucasian", ".xlsx");
+
+        file.transferTo(dest);
+
+        jobLauncher.run(resumenJob, new JobParametersBuilder()
+                 .addString("inputResource", "file:" + dest.getAbsolutePath())
+                 .addString("csvDirectory", "/tmp/")
+                 .addString("csvFile", "resumen.csv")
+                 .addDate("currentRun", new Date())
+                 .toJobParameters());
+
         return "Hola mundo";
     }
 
